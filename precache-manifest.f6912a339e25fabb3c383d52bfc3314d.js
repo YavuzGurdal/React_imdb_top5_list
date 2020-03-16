46977837bbf011c24bd33a91400200398206e345
Yavuzgrdl <32365733+Yavuzgrdl@users.noreply.github.com>
2020-03-16 19:28:47 +0100

    Updates

---
+++ precache-manifest.f6912a339e25fabb3c383d52bfc3314d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "879eb4079a6dee4dbd6e92fd8fb931b8",
+    "url": "/React_imdb_top5_list/index.html"
+  },
+  {
+    "revision": "5b3a83ccd669e5f23594",
+    "url": "/React_imdb_top5_list/static/css/2.47e06e2e.chunk.css"
+  },
+  {
+    "revision": "1f03b44f0d78c3e52862",
+    "url": "/React_imdb_top5_list/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "5b3a83ccd669e5f23594",
+    "url": "/React_imdb_top5_list/static/js/2.cd72a272.chunk.js"
+  },
+  {
+    "revision": "eff3b4d7e4bb0087ce53139cd90bc880",
+    "url": "/React_imdb_top5_list/static/js/2.cd72a272.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1f03b44f0d78c3e52862",
+    "url": "/React_imdb_top5_list/static/js/main.704ec5f4.chunk.js"
+  },
+  {
+    "revision": "c477107eae90c0899e07",
+    "url": "/React_imdb_top5_list/static/js/runtime-main.b9650071.js"
+  }
+]);
